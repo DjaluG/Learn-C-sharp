957217641e29e6488a4d007b38c3063c65e74ec0
DjaluGalang <110319811+TezukaKarlsefni@users.noreply.github.com>
2022-10-28 10:13:29 +0700

    Add files via upload

---
+++ Bab1.cs
@@ -0,0 +1,22 @@
+
+using System;
+
+public class HelloWorld
+{
+    //method pertama
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Nama : Djalu Galang"); //string
+        Console.WriteLine ("Nis : 12108393"); //string
+        Console.WriteLine ("Rombel : PPLG XI-4"); //string
+        Console.WriteLine (1234567890); //integear
+        Second(); //memanggil method kedua
+    }
+    
+    //method kedua
+    public static void Second()
+    {
+        string SISWA = "LIGMA"; //variabel
+        Console.WriteLine(SISWA);
+    }
+}
